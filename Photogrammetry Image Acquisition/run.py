a2e197ae04b0946e4073247173ab7d7fd62fef55
Fabian <mail@fabian-duerkop.de>
2021-06-10 16:31:41 +0200

    Better Readme and added more variables to Image Acquisition

--- Photogrammetry Image Acquisition/run.py
+++ Photogrammetry Image Acquisition/run.py
@@ -1,23 +1,29 @@
 import pyautogui
 import time
 
-i = 0
+# Used for https://www.heroforge.com/
+
 
 # Type in your start position of the cursor (X, Y)
 posX = 881
 posY = 459
 
+# Put in the position of the upper left corner of the desired screenshot area and the width / height
+screenRegionX = 499
+screenRegionY = 120
+screenRegionWidth = 1321
+screenRegionHeight = 820
+
 # Comment out when done getting coordinates
 while True:
     print(pyautogui.position())
 
+i = 0
 time.sleep(2)
 pyautogui.FAILSAFE = True
 while i < 70:
 
-    # Put in the position of the upper left corner of the desired screenshot area and the width / height
-    #                                         x    y    w     h
-    screenshot = pyautogui.screenshot(region=(719, 113, 1000, 900)).save(
+    screenshot = pyautogui.screenshot(region=(screenRegionX, screenRegionY, screenRegionWidth, screenRegionHeight)).save(
         'scanImage' + str(i) + '.jpg')
     i += 1
 
